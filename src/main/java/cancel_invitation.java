41a07d48c9feb64a4f6df86c81b72063b3f44f29
harish <harishram.n@gmail.com>
2025-11-16 19:20:50 -0500

    completed cancel_invitation class

--- src/main/java/cancel_invitation.java
+++ src/main/java/cancel_invitation.java
@@ -5,7 +5,7 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Updates.*;
-
+//mongodb
 
 
 
