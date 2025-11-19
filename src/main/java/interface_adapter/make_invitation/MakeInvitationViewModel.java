102e9f0db28a15ae30daa14c2bedc431dd22898a
Q1yuu <qiyu13907157177@gmail.com>
2025-11-19 16:16:07 -0500

    makeinvitation viewmodel updated

--- src/main/java/interface_adapter/make_invitation/MakeInvitationViewModel.java
+++ src/main/java/interface_adapter/make_invitation/MakeInvitationViewModel.java
@@ -1,4 +1,32 @@
 package interface_adapter.make_invitation;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class MakeInvitationViewModel {
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    private String successMessage = "";
+    private String failMessage = "";
+
+    public String getSuccessMessage() {
+        return successMessage;
+    }
+    public void setSuccessMessage(String successMessage) {
+        this.successMessage = successMessage;
+    }
+    public String getFailMessage() {
+        return failMessage;
+    }
+    public void setFailMessage(String failMessage) {
+        this.failMessage = failMessage;
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        support.removePropertyChangeListener(listener);
+    }
+
 }
