34c6c0c096d4ac5691e3294674605c0e91923863
Q1yuu <qiyu13907157177@gmail.com>
2025-11-19 16:31:05 -0500

    makeinvitation controller and presenter

--- src/main/java/interface_adapter/make_invitation/MakeInvitationController.java
+++ src/main/java/interface_adapter/make_invitation/MakeInvitationController.java
@@ -1,4 +1,30 @@
 package interface_adapter.make_invitation;
 
-public class MakeInvitationController {
+import use_case.make_invitation.MakeInvitationInputBoundary;
+import use_case.make_invitation.MakeInvitationInputData;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+public class MakeInvitationController{
+    private final MakeInvitationInputBoundary interactor;
+
+    public MakeInvitationController(MakeInvitationInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+    public void onConfirm(String course,
+                          String description,
+                          LocalDate date,
+                          LocalTime start,
+                          LocalTime end,
+                          String mode,
+                          String location,
+                          Integer occupancy) {
+        MakeInvitationInputData req = new MakeInvitationInputData(
+                course, description, date, start, end, mode, location, occupancy
+        );
+        interactor.execute(req);
+    }
 }
+
--- src/main/java/interface_adapter/make_invitation/MakeInvitationPresenter.java
+++ src/main/java/interface_adapter/make_invitation/MakeInvitationPresenter.java
@@ -1,4 +1,25 @@
 package interface_adapter.make_invitation;
 
-public class MakeInvitationPresenter {
+import use_case.make_invitation.MakeInvitationOutputBoundary;
+import use_case.make_invitation.MakeInvitationOutputData;
+
+public class MakeInvitationPresenter implements MakeInvitationOutputBoundary {
+    private final MakeInvitationViewModel vm;
+
+    public MakeInvitationPresenter(MakeInvitationViewModel vm) {
+        this.vm = vm;
+    }
+
+    @Override
+    public void presentSuccess(MakeInvitationOutputData response) {
+        vm.setFailMessage("");
+        vm.setSuccessMessage(response.getMessage());
+
+    }
+
+    @Override
+    public void presentFailure(String errorMessage) {
+        vm.setSuccessMessage("");
+        vm.setFailMessage(errorMessage);
+    }
 }
