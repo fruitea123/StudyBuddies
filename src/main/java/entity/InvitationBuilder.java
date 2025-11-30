463d55a24d12aeee4420051d947b2070ca5d04cb
Q1yuu <qiyu13907157177@gmail.com>
2025-11-30 14:41:01 -0500

    fix style

--- src/main/java/entity/InvitationBuilder.java
+++ src/main/java/entity/InvitationBuilder.java
@@ -16,91 +16,7 @@ public class InvitationBuilder {
     private String location;
     private Integer capacity;
     private User owner;
-    private List<User> participants =  new ArrayList<>();
-
-    // Setter
-    public InvitationBuilder course(String v){
-        this.course = v;
-        return this;
-    }
-    public InvitationBuilder description(String v){
-        this.description = v;
-        return this;
-    }
-    public InvitationBuilder date(LocalDate v){
-        this.date = v;
-        return this;
-    }
-    public InvitationBuilder startTime(LocalTime v){
-        this.startTime = v;
-        return this;
-    }
-    public InvitationBuilder endTime(LocalTime v){
-        this.endTime = v;
-        return this;
-    }
-    public InvitationBuilder mode(String v){
-        this.mode = v;
-        return this;
-    }
-    public InvitationBuilder location(String v){
-        this.location = v;
-        return this;
-    }
-    public InvitationBuilder capacity(Integer v){
-        this.capacity = v;
-        return this;
-    }
-    public InvitationBuilder owner(User u) {
-        this.owner = u;
-        return this;
-    }
-    public InvitationBuilder addParticipant(User u) {
-        if (u != null) this.participants.add(u);
-        return this;
-    }
-    public InvitationBuilder participants(Collection<User> users) {
-        if (users != null) {
-            this.participants.addAll(users);
-        }
-        return this;
-    }
-
-    // getter
-    public String getCourse(){
-        return course;
-    }
-    public String getDescription(){
-        return description;
-    }
-    public LocalDate getDate(){
-        return date;
-    }
-    public LocalTime getStartTime(){
-        return startTime;
-    }
-    public LocalTime getEndTime(){
-        return endTime;
-    }
-    public String getMode(){
-        return mode;
-    }
-    public String getLocation(){
-        return location;
-    }
-    public Integer getCapacity(){
-        return capacity;
-    }
-    public User getOwner(){
-        return owner;
-    }
-    public List<User> getParticipants(){
-        return participants;
-    }
-
-    public Invitation build() {
-        return new Invitation(this);
-    }
+    private List<User> participants = new ArrayList<>();
 
     public static InvitationBuilder from(Invitation src) {
         return new InvitationBuilder()
@@ -113,6 +29,109 @@ public class InvitationBuilder {
                 .capacity(src.getCapacity())
                 .owner(src.getOwner())
                 .participants(src.getParticipants());
+    }
+
+    // Setter
+    public InvitationBuilder course(String v) {
+        this.course = v;
+        return this;
+    }
+
+    public InvitationBuilder description(String v) {
+        this.description = v;
+        return this;
+    }
+
+    public InvitationBuilder date(LocalDate v) {
+        this.date = v;
+        return this;
+    }
+
+    public InvitationBuilder startTime(LocalTime v) {
+        this.startTime = v;
+        return this;
+    }
+
+    public InvitationBuilder endTime(LocalTime v) {
+        this.endTime = v;
+        return this;
+    }
+
+    public InvitationBuilder mode(String v) {
+        this.mode = v;
+        return this;
+    }
+
+    public InvitationBuilder location(String v) {
+        this.location = v;
+        return this;
+    }
+
+    public InvitationBuilder capacity(Integer v) {
+        this.capacity = v;
+        return this;
+    }
+
+    public InvitationBuilder owner(User u) {
+        this.owner = u;
+        return this;
+    }
+
+    public InvitationBuilder addParticipant(User u) {
+        if (u != null) this.participants.add(u);
+        return this;
+    }
+
+    public InvitationBuilder participants(Collection<User> users) {
+        if (users != null) {
+            this.participants.addAll(users);
+        }
+        return this;
+    }
+
+    // getter
+    public String getCourse() {
+        return course;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public Invitation build() {
+        return new Invitation(this);
     }
 
 
