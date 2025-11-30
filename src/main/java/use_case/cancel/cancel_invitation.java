dc1ba9746bf35746ab678cfe3b90fa9b1bcd24a6
harish1010-code <harishram.n@gmail.com>
2025-11-30 12:57:44 -0500

    Calendar (#20)
    * Added mongodb-driver to pom.xml
    
    * implemented leave function
    
    * implemented delete function
    
    * completed cancel_invitation class
    
    * moved to the use case/interactor
    
    * created calendar class
    
    ---------
    
    Co-authored-by: nooramad <noor.amad@mail.utoronto.ca>

---
+++ src/main/java/use_case/cancel/cancel_invitation.java
@@ -0,0 +1,75 @@
+package use_case.cancel;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
+//mongodb
+
+
+
+public class cancel_invitation {
+    private MongoCollection<Document> invitationsCollection;
+    private MongoCollection<Document> usersCollection;
+    private String username;
+    private String ownername;
+    private String startTime;
+    private String endTime;
+
+    public cancel_invitation()
+    {
+        MongoClient client=MongoClients.create("mongodb://localhost:27017");//(sample localhost) this would change based on which laptop the db is created
+        MongoDatabase database=client.getDatabase("StudyPool");//our database name which is studypool
+        invitationsCollection=database.getCollection("Invitations");
+        usersCollection=database.getCollection("users");
+    }
+    public void setUserName(String username) { this.username = username; }
+    public void setOwnerName(String ownername) { this.ownername = ownername; }
+    public void setStartTime(String startTime) { this.startTime = startTime; }
+    public void setEndTime(String endTime) { this.endTime = endTime; }
+
+    public void leave()
+    {
+        //removes from user's invitation list
+        usersCollection.updateOne(eq("username",username),
+                pull("invitations",and(
+                        eq("startTime",startTime),
+                        eq("endTime",endTime)
+                  )
+                )
+        );
+
+
+    }
+    public void delete()
+    {
+        //Delete invitation from onwer invitation page
+        invitationsCollection.deleteOne(and(  eq("startTime", startTime),
+                        eq("endTime", endTime),
+                        eq("ownername", ownername)
+                )
+        );
+        //remove the same invitation from every user's invitation page
+        usersCollection.updateMany(
+                elemMatch("invitations",
+                        and(
+                                eq("startTime", startTime),
+                                eq("endTime", endTime)
+                        )
+                ),
+                pull("invitations",
+                        and(
+                                eq("startTime", startTime),
+                                eq("endTime", endTime)
+                        )
+                )
+        );
+
+    }
+
+
+
+}
