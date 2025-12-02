2aaa505ca183a4272089eeacc04bfe8ce7e389e0
Jiya Bajaj <bajaj.jiya19@gmail.com>
2025-12-02 13:54:45 -0500

    acceptInivitationController checkstyle

--- src/main/java/interface_adapter/accept/AcceptInvitationController.java
+++ src/main/java/interface_adapter/accept/AcceptInvitationController.java
@@ -3,15 +3,36 @@ package interface_adapter.accept;
 import use_case.accept.AcceptInvitationInputBoundary;
 import use_case.accept.AcceptInvitationInputData;
 
+/**
+ * Controller for the Accept Invitation use case.
+ *
+ * <p>Translates UI input (invitation id and username) into an
+ * {@link AcceptInvitationInputData} object and delegates the
+ * request to the input boundary.
+*/
 public class AcceptInvitationController {
-    private final AcceptInvitationInputBoundary inputBoundary;
+  private final AcceptInvitationInputBoundary inputBoundary;
+  /**
+     * Constructs a controller with the given accept‑invitation input boundary.
+     *
+     * @param inputBoundary use case interactor that will handle accept requests.
+  */
 
-    public AcceptInvitationController(AcceptInvitationInputBoundary inputBoundary) {
-        this.inputBoundary = inputBoundary;
-    }
+  public AcceptInvitationController(AcceptInvitationInputBoundary inputBoundary) {
+    this.inputBoundary = inputBoundary;
+  }
+  /**
+     * Handles a request from the UI to accept an invitation.
+     *
+     * <p>Wraps the raw parameters in an {@link AcceptInvitationInputData}
+     * object and passes it to the use case interactor.
+     *
+     * @param invitationId identifier of the invitation to be accepted.
+     * @param username     user who is accepting the invitation.
+  */
 
-    public void acceptInvitation(String invitationId, String username) {
-        AcceptInvitationInputData inputData = new AcceptInvitationInputData(username, invitationId);
-        inputBoundary.acceptInvitation(inputData);
-    }
+  public void acceptInvitation(String invitationId, String username) {
+    AcceptInvitationInputData inputData = new AcceptInvitationInputData(username, invitationId);
+    inputBoundary.acceptInvitation(inputData);
+  }
 }
