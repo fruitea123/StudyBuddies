2ceabda71b5fc55cac0cdf6c0b1df5db4e469650
harish <harishram.n@gmail.com>
2025-12-02 17:32:19 -0500

    changed tests

--- src/test/java/usecase/cancel/CancelInvitationTest.java
+++ src/test/java/usecase/cancel/CancelInvitationTest.java
@@ -4,16 +4,21 @@ import data_access.MongoCancelInvitationDAO;
 
 //test
 public class CancelInvitationTest {//cancel invitation test
-
     public static void main(String[] args) {
 
         MongoCancelInvitationDAO dao = new MongoCancelInvitationDAO();
         CancelInvitation cancel = new CancelInvitation(dao);
 
-        cancel.setOwnerName("john");
+        cancel.setOwnerName("Tim@mail.utoronto.ca");
         cancel.delete();
 
-        cancel.setUsername("max");
+        cancel.setOwnerName("");
+        cancel.delete();
+
+        cancel.setUsername("");
+        cancel.leave();
+
+        cancel.setUsername("max@mail.utoronto.ca");
         cancel.leave();
 
         System.out.println("Test complete.");
