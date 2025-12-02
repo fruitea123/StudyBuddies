dd039dabca605aa02e880d4211f30499ff8097af
Jiya Bajaj <bajaj.jiya19@gmail.com>
2025-12-02 13:55:54 -0500

    acceptInivitationInputBoundary checkstyle

--- src/main/java/use_case/accept/AcceptInvitationInputBoundary.java
+++ src/main/java/use_case/accept/AcceptInvitationInputBoundary.java
@@ -1,6 +1,14 @@
 package use_case.accept;
 
+/**
+ * Defines methods to handle the input of the AcceptInvitation use case.
+ */
 public interface AcceptInvitationInputBoundary {
-    void acceptInvitation(AcceptInvitationInputData inputData);
+  /**
+     * Called when the accept‑invitation use case receives an input.
+     *
+     * @param inputData containing the user's and owner's username.
+  */
+  void acceptInvitation(AcceptInvitationInputData inputData);
 }
 
